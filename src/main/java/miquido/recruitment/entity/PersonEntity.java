f9583c1dc32fd8e5f408976ba16e5303674f54df
Artur Hamernik <hamernik18@gmail.com>
2023-12-13 01:21:05 +0100

    table name

--- src/main/java/miquido/recruitment/entity/PersonEntity.java
+++ src/main/java/miquido/recruitment/entity/PersonEntity.java
@@ -1,5 +1,6 @@
 package miquido.recruitment.entity;
 
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -13,6 +14,7 @@ import java.math.BigDecimal;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Table(name = "person")
 @Entity
 public class PersonEntity {
 
